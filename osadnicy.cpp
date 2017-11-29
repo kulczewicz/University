d4c8ae4a807c8323cd0c83d2f41950c5468b601c
Oleś Kulczewicz <15707597+kulczewicz@users.noreply.github.com>
2017-11-29 22:37:35 +0100

    Add files via upload

--- osadnicy.cpp
+++ osadnicy.cpp
@@ -3,14 +3,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-using namespace std;
-
 int mysliwi;
 int kucharze;
 int zwierzyna;
 int pozywienie;
-pthread_mutex_t semaforZwierzyny;
-pthread_mutex_t semaforPozywienia;
+pthread_mutex_t mutexZwierzyny;
+pthread_mutex_t mutexPozywienia;
 
 void *polowanie(void*) 
 {
@@ -20,18 +18,19 @@ void *polowanie(void*)
       {
          zwierzyna++;
       }
-      pthread_mutex_lock(&semaforPozywienia);
+      pthread_mutex_lock(&mutexPozywienia);
       if (pozywienie == 0) 
       {
          mysliwi--;
-         pthread_mutex_unlock(&semaforPozywienia);
+         pthread_mutex_unlock(&mutexPozywienia);
+         //printf("Myśliwy opuszcza dolinę...\nwatek mysliwego ---- mysliwi: %d kucharze: %d zwierzyna: %d pozywienie: %d dzien numer %d\n", mysliwi, kucharze, zwierzyna, pozywienie, i);
          break;
       }
       else
          pozywienie--;
-      pthread_mutex_unlock(&semaforPozywienia);
-      printf("watek mysliwego ---- mysliwi: %d kucharze: %d zwierzyna: %d pozywienie: %d obieg petli: %d\n", mysliwi, kucharze, zwierzyna, pozywienie, i);
-      usleep(100000);
+      pthread_mutex_unlock(&mutexPozywienia);
+      //printf("watek mysliwego ---- mysliwi: %d kucharze: %d zwierzyna: %d pozywienie: %d dzien numer %d\n", mysliwi, kucharze, zwierzyna, pozywienie, i);
+      usleep(1000);
    }
    pthread_exit(NULL);
 }
@@ -40,35 +39,32 @@ void *gotowanie(void*)
 {
    for (int i=0; i<200; i++)
    {
-      pthread_mutex_lock(&semaforZwierzyny);
+      pthread_mutex_lock(&mutexZwierzyny);
       if (zwierzyna>0)
       {
          zwierzyna--;
          pozywienie += rand()%6+1;
       }
-      pthread_mutex_unlock(&semaforZwierzyny);
-      pthread_mutex_lock(&semaforPozywienia);
+      pthread_mutex_unlock(&mutexZwierzyny);
+      pthread_mutex_lock(&mutexPozywienia);
       if (pozywienie == 0) 
       {
          kucharze--;
-         pthread_mutex_unlock(&semaforPozywienia);
+         pthread_mutex_unlock(&mutexPozywienia);
+         //printf("Kucharz opuszcza dolinę...\nwatek kucharza  ---- mysliwi: %d kucharze: %d zwierzyna: %d pozywienie: %d dzien numer %d\n", mysliwi, kucharze, zwierzyna, pozywienie, i);
          break;
       }
       else
          pozywienie--;
-      pthread_mutex_unlock(&semaforPozywienia);
-      printf("watek kucharza ----- mysliwi: %d kucharze: %d zwierzyna: %d pozywienie: %d  obieg petli: %d\n", mysliwi, kucharze, zwierzyna, pozywienie, i);
-      usleep(100000);
+      pthread_mutex_unlock(&mutexPozywienia);
+      //printf("watek kucharza  ---- mysliwi: %d kucharze: %d zwierzyna: %d pozywienie: %d dzien numer %d\n", mysliwi, kucharze, zwierzyna, pozywienie, i);
+      usleep(1000);
    }
    pthread_exit(NULL);
 }
 
 int main (int argc, char** argv) 
 {
-   printf("argv[0]: %d\n", atoi(argv[1]));
-   printf("argv[1]: %d\n", atoi(argv[2]));
-   printf("argv[2]: %d\n", atoi(argv[3]));
-   printf("argv[3]: %d\n", atoi(argv[4]));
    mysliwi = atoi(argv[1]);
    kucharze = atoi(argv[2]);
    zwierzyna = atoi(argv[3]);
@@ -77,8 +73,8 @@ int main (int argc, char** argv)
    pthread_t mysliwi_w[atoi(argv[1])];
    pthread_t kucharze_w[atoi(argv[2])];  
    
-   pthread_mutex_init(&semaforZwierzyny, NULL);
-   pthread_mutex_init(&semaforPozywienia, NULL);
+   pthread_mutex_init(&mutexZwierzyny, NULL);
+   pthread_mutex_init(&mutexPozywienia, NULL);
 
    for (int i=0; i<atoi(argv[1]); i++)
    {
@@ -96,8 +92,8 @@ int main (int argc, char** argv)
    {
       pthread_join(kucharze_w[i], NULL);
    }
-   pthread_mutex_destroy(&semaforPozywienia);
-   pthread_mutex_destroy(&semaforZwierzyny);
+   pthread_mutex_destroy(&mutexPozywienia);
+   pthread_mutex_destroy(&mutexZwierzyny);
 
    printf("mysliwi: %d\nkucharze: %d\nzwierzyna: %d\npozywienie: %d\n", mysliwi, kucharze, zwierzyna, pozywienie);
    return 0;
